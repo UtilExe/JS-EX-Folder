3503f5ddd2bd4ae966c85deafb909d3bb736009e
UtilExe <laymasterlolz@gmail.com>
2021-02-23 22:05:11 +0100

    Exercise 2a and 2b

---
+++ JS-ESX-Day3/exercises/exercise2-a.js
@@ -0,0 +1,31 @@
+// 2 Chaining promises (fetch requests), and why GraphQL is cool ;-)
+const fetch = require('node-fetch');
+const url = 'https://swapi.dev/api/people/';
+
+function getPlanetforFirstSpeciesInFirstMovieForPerson(id) {
+    fetch(url + id)
+        .then(res => res.json())
+        .then(data => {
+            console.log("Name:", data.name)
+            fetch(data.films[0])
+                .then(res => res.json())
+                .then(data1 => {
+                    console.log("Movie Name:", data1.title)
+                    fetch(data1.species[0])
+                        .then(res => res.json())
+                        .then(data2 => {
+                            console.log("Species:", data2.name)
+                            fetch(data2.homeworld)
+                                .then(res => res.json())
+                                .then(data3 => {
+                                    console.log("Homeworld:", data3.name)
+                                })
+                        })
+                })
+        }).catch((err) => {
+            console.log("Error: ", err);
+        })
+}
+
+
+getPlanetforFirstSpeciesInFirstMovieForPerson(1)
