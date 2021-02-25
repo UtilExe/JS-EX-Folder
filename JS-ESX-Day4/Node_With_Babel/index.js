9eed35bc6c57725a45db588fe20eeec83fc20298
UtilExe <laymasterlolz@gmail.com>
2021-02-25 10:47:06 +0100

    Using node with Babel exercise task.

---
+++ JS-ESX-Day4/Node_With_Babel/index.js
@@ -0,0 +1,20 @@
+//const fetch = require('node-fetch');
+import fetch from "node-fetch"
+
+function messageDelay(msg, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const upperCased = msg.toUpperCase();
+            resolve(upperCased)
+        }, delay)
+    })
+}
+
+const promises = [
+    messageDelay("Hi", 1300),
+    messageDelay("Hi Class", 700),
+    messageDelay("promises are cool", 500),
+    messageDelay("hello world", 1700),
+]
+
+Promise.any(promises).then((upperCased) => console.log(upperCased));
