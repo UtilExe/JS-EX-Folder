3a078b37e2c6125b102486ed46a35a3d3aca6340
UtilExe <laymasterlolz@gmail.com>
2021-04-07 17:48:10 +0200

    Implement facade and router based on MongoDB. To-do: Bit clean-up, think about the DTO part, implement try and catch. Perhaps ask Lars if intended he didnt use.

---
+++ Period2/day3/fullstack_startcode/src/facades/friendFacade.ts
@@ -0,0 +1,124 @@
+import { IFriend } from '../interfaces/IFriend';
+import { Db, Collection } from "mongodb";
+import bcrypt from "bcryptjs";
+import { ApiError } from '../errors/apiError';
+import Joi, { ValidationError } from "joi"
+
+const BCRYPT_ROUNDS = 10;
+
+const USER_INPUT_SCHEMA = Joi.object({
+  firstName: Joi.string().min(2).max(40).required(),
+  lastName: Joi.string().min(2).max(50).required(),
+  password: Joi.string().min(4).max(30).required(),
+  email: Joi.string().email().required()
+})
+
+class FriendsFacade {
+  db: Db
+  friendCollection: Collection
+
+  constructor(db: Db) {
+    this.db = db;
+    this.friendCollection = db.collection("friends");
+  }
+
+  /**
+   * 
+   * @param friend 
+   * @throws ApiError if validation fails
+   */
+  // Completed method
+  async addFriend(friend: IFriend): Promise<{ id: string }> {
+    const status = USER_INPUT_SCHEMA.validate(friend);
+    if (status.error) {
+      throw new ApiError(status.error.message, 400)
+    }
+    const hashedpw = await bcrypt.hash(friend.password, BCRYPT_ROUNDS);
+    const f = { ...friend, password: hashedpw }
+
+   await this.friendCollection.insertOne({
+     firstName: f.firstName,
+     lastName: f.lastName,
+     email: f.email,
+     password: f.password,
+     role: "user"
+    })
+    return this.friendCollection.findOne({email: f.email})
+}
+
+  /**
+   * @param email 
+   * @param friend 
+   * @throws ApiError if validation fails or friend was not found
+   */
+  // Completed method
+  async editFriend(email: string, friend: IFriend): Promise<{ modifiedCount: number }> {
+    const status = USER_INPUT_SCHEMA.validate(friend);
+    if (status.error) {
+      throw new ApiError(status.error.message, 400)
+    }
+    const hashedpw = await bcrypt.hash(friend.password, BCRYPT_ROUNDS);
+    const f = { ...friend, password: hashedpw }
+
+    await this.friendCollection.updateOne(
+    {email: email},
+    {$set: {
+     email: f.email,
+     firstName: f.firstName,
+     lastName: f.lastName,
+     password: f.password,
+    }})
+    return this.friendCollection.findOne({email: f.email})
+  }
+
+  /**
+   * 
+   * @param friendEmail 
+   * @returns true if deleted otherwise false
+   */
+  // Completed method
+  async deleteFriend(friendEmail: string): Promise<boolean> {
+    const count = await this.friendCollection.deleteOne(
+      {email: friendEmail}
+    )
+    if (count.deletedCount && count.deletedCount > 0) {
+    return Promise.resolve(true)
+  } else {
+    return Promise.resolve(false)
+  }
+}
+
+  async getAllFriends(): Promise<Array<IFriend>> {
+    const users: unknown = await this.friendCollection.find({}).toArray();
+    return users as Array<IFriend>
+  }
+  
+  /**
+   * 
+   * @param friendEmail 4
+   * @returns 
+   * @throws ApiError if not found
+   */
+  // Completed method
+  async getFrind(friendEmail: string): Promise<IFriend> {
+     const friend: IFriend = await this.friendCollection.findOne({email:friendEmail})
+     return Promise.resolve(friend)
+  }
+
+  /**
+   * Use this method for authentication
+   * @param friendEmail 
+   * @param password 
+   * @returns the user if he could be authenticated, otherwise null
+   */
+  async getVerifiedUser(friendEmail: string, password: string): Promise<IFriend | null> {
+    const friend: IFriend = await this.friendCollection.findOne({ email: friendEmail })
+    if (friend && await bcrypt.compare(password, friend.password)) {
+      return friend
+    }
+    return Promise.resolve(null)
+  }
+
+}
+
+export default FriendsFacade;
